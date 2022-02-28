c1f88279a18ef984718392a27865ce407f88f541
Anton Wilhelm <anton.wilhelm@here.com>
2022-02-28 13:08:46 +0100

    Switch from travis to GitHub actions
    Signed-off-by: Anton Wilhelm <anton.wilhelm@here.com>

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -5,11 +5,9 @@ export SONATYPE_USERNAME=$OSSRH_USERNAME
 export SONATYPE_PASSWORD=$OSSRH_PASSWORD
 export PGP_PASSPHRASE=$GPG_PASSPHRASE
 
-# Decrypt gpg key
-openssl aes-256-cbc -K $encrypted_81c98acad902_key -iv $encrypted_81c98acad902_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
-
 # Import gpg key
-gpg --fast-import .travis/codesigning.asc
+echo $GPG_PRIVATE_KEY > private.key
+gpg --fast-import private.key
 
 # Deploy to Maven Central
 sbt sonatypeDropAll
